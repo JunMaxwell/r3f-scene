2bc58c5138abd3dcda70349298916e6d711528fb
Y.DoThien <haloany123@gmail.com>
2024-08-18 14:32:54 +0700

    feat: intro and main

---
+++ src/intro.jsx
@@ -0,0 +1,17 @@
+import { cloneElement, useState } from "react"
+
+export default function Intro({ children }) {
+  const [clicked, setClicked] = useState(false)
+  return (
+    <>
+      {cloneElement(children, { ready: clicked })}
+      <div className={`fullscreen bg ready"} ${clicked && "clicked"}`}>
+        <div className="stack">
+          <a href="#" onClick={() => setClicked(true)}>
+            {"click to continue"}
+          </a>
+        </div>
+      </div>
+    </>
+  )
+}
