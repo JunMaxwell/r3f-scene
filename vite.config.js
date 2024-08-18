58bd1f22c055622724a1c5f4427c426c2e5e533f
Y.DoThien <haloany123@gmail.com>
2024-08-18 15:26:37 +0700

    chore: add env

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/r3f-scene/"
-})
+export default ({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  return defineConfig({
+    plugins: [react()],
+    base: process.env.VITE_PUBLIC_URL || '/',
+  });
+}
